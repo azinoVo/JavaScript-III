e4fcdcc1c4f7b7cd3c976c3527f7dc36b4655c72
azinoVo <azinovo22@gmail.com>
2019-04-03 17:53:04 -0500

    finish this.js section

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,67 @@
-/* The for principles of "this";
+/* The four principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding ~ when in global scope, this will point to the whole window object
+or forest.
+* 2. Implicit Binding ~ occurs when using dot notation to invoke a function; whatever
+is left of the dot is "this"
+* 3. Explicit Binding ~ occurs when using the call(), apply(), and bind() methods;
+"this" is passed with the method as an argument. 
+* 4. New Binding ~ the object created and returned by a constructor function is the
+target of the "this" keyword;
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
+// Principle 1 - Windows Binding
 
-// code example for Window Binding
+function windowBind (num1,num2) {
+    console.log(this);
+    return num1 * num2 ; 
+};
 
-// Principle 2
+windowBind(1,4);
 
-// code example for Implicit Binding
 
-// Principle 3
+// Principle 2 - Implicit Binding
 
-// code example for New Binding
+const fruitSalad = {
+    fruit1:"apple",
+    fruit2:"pineapple",
+    topping:"honey",
+    stir: function() {
+        console.log("Stir " + this.topping + " and your favorite fruits together.");
+    }
+};
 
-// Principle 4
+fruitSalad.stir();
 
-// code example for Explicit Binding
+// Principle 3 - Explicit Binding
+
+const gamesPlayed = function(game1,game2,game3){
+    console.log(`I am ${this.tisI} and I like to play ${game1}, ${game2}, 
+    and ${game3}. `)
+};
+
+const me = {
+    tisI: "Nguyen",
+    tisDo: "play Games"
+}
+
+const games = ["Path of Exile","Shadowverse", "Epic Seven"];
+
+gamesPlayed.apply(me,games);
+
+
+// Principle 4 - New Binding
+
+const orderInsects = function(order,name,flight) {
+    this.order = order,
+    this.name = name,
+    this.flight = flight
+} 
+
+const Lepidoptera = new orderInsects ("Lepidoptera", "Butterflies/Moths", "Winged and Wingless Stage");
+console.log(Lepidoptera);
+
+// this is attached to the this = {} when invoked by the new keyword?
